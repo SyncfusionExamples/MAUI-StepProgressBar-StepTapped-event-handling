0c901838369e98c0d0bccf96b5c4e22b04794ed5
TamilarasanGunasekaran <tamilarasan.g@syncfusion.com>
2024-04-17 19:08:01 +0530

    Sample added for Step progress bar step tapped event handling

---
+++ StepProgressBarStatusSteppedEvent/StepProgressBarStatusSteppedEvent/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StepProgressBarStatusSteppedEvent
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ StepProgressBarStatusSteppedEvent/StepProgressBarStatusSteppedEvent/ViewModel/StepProgressBarViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.Maui.ProgressBar;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepProgressBarStatusSteppedEvent;
+
+/// <summary>
+/// Represents the view model for the step progress bar functionality.
+/// </summary>
+public class StepProgressBarViewModel
+{
+    /// <summary>
+    /// The Step progress bar item collection.
+    /// </summary>
+    private ObservableCollection<StepProgressBarItem> stepProgressItem;
+
+    /// <summary>
+    /// The Step progress bar item collection.
+    /// </summary>
+    public ObservableCollection<StepProgressBarItem> StepProgressItem
+    {
+        get
+        {
+            return stepProgressItem;
+        }
+        set
+        {
+            stepProgressItem = value;
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StepProgressBarViewModel"/> class.
+    /// </summary>
+    public StepProgressBarViewModel()
+    {
+        stepProgressItem = new ObservableCollection<StepProgressBarItem>();
+        stepProgressItem.Add(new StepProgressBarItem() { PrimaryText = "Cart" });
+        stepProgressItem.Add(new StepProgressBarItem() { PrimaryText = "Address" });
+        stepProgressItem.Add(new StepProgressBarItem() { PrimaryText = "Delivery" });
+        stepProgressItem.Add(new StepProgressBarItem() { PrimaryText = "Ordered" });
+    }
+}
+
